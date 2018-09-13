998e0d59a28edf01765b666ba1b3409743cb55bf
k15015kk <aitk15015kk@outlook.com>
2018-09-13 11:43:55 +0900

    first commit

---
+++ app/src/main/java/nagoya/kyoto/quakeshare/MainSelectActivity.kt
@@ -0,0 +1,12 @@
+package nagoya.kyoto.quakeshare
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainSelectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_select)
+    }
+}
