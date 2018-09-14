21d28b33ba5e307c1bc51d2ced1f6d9157de3380
k15015kk <aitk15015kk@outlook.com>
2018-09-14 19:26:29 +0900

    レイアウトの実装とボタンの実装

--- app/src/main/java/nagoya/kyoto/quakeshare/MainSelectActivity.kt
+++ app/src/main/java/nagoya/kyoto/quakeshare/MainSelectActivity.kt
@@ -2,11 +2,24 @@ package nagoya.kyoto.quakeshare
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainSelectActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_select)
+
+        val weakQuakeButton : Button = findViewById(R.id.weakQuakeButton)
+        val strongQuakeButton: Button = findViewById(R.id.strongQuakeButton)
+
+        weakQuakeButton.setOnClickListener{
+            Toast.makeText(this,"よわいボタンが押されました",Toast.LENGTH_SHORT).show()
+        }
+
+        strongQuakeButton.setOnClickListener{
+            Toast.makeText(this,"つよいボタンが押されました",Toast.LENGTH_SHORT).show()
+        }
     }
 }
